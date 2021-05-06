56f5ccd9bcb0cce7ee3fa880a8560ee1ee6cd1d9
chenmugui <412506409@qq.com>
2021-05-06 19:38:55 +0800

    提交2

---
+++ scripts/test04_mis_audit.py
@@ -0,0 +1,39 @@
+import uiAutoTestHmtt.page
+from uiAutoTestHmtt.page.page_in import PageIn
+from uiAutoTestHmtt.tools.get_driver import GetDriver
+from uiAutoTestHmtt.tools.get_log import GetLog
+
+log = GetLog.get_logger()
+
+
+class TestMisAudit:
+    # 1. 初始化
+    def setup_class(self):
+        # 1. 获取driver
+        driver = GetDriver.get_web_driver(uiAutoTestHmtt.page.url_mis)
+        # 2. 获取统一入口类对象
+        self.page_in = PageIn(driver)
+        # 3. 调用成功登录依赖方法
+        self.page_in.page_get_PageMisLogin().page_mis_login_success()
+        # 4. 获取PageMisAudit对象
+        self.audit = self.page_in.page_get_PageMisAudit()
+
+    # 2. 结束
+    def teardown_class(self):
+        # 关闭driver
+        GetDriver.quit_web_driver()
+
+    # 3. 审核文章业务测试方法
+    def test_mis_audit(self, title=uiAutoTestHmtt.page.title, channel=uiAutoTestHmtt.page.channle):
+        # 调用审核文章业务方法
+        self.audit.page_mis_audit(title, channel)
+        try:
+            # 断言
+            assert self.audit.page_assert_audit()
+        except Exception as e:
+            # 1. 日志
+            log.error(e)
+            # 2. 截图
+            self.audit.base_get_img()
+            # 3. 抛异常
+            raise
